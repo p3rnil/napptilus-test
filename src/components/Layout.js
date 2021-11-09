db78915ac2d523a7a5835b7474820a6058e88fad
P3rni1 <nicolasacostap@gmail.com>
2021-11-09 16:45:08 +0100

    Layout structure 🎨

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,29 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Header from './Header'
+import List from './List'
+import SearchBar from './SearchBar'
+
 const Layout = () => {
-  return <span>Layout</span>
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateRows: 'repeat(3, 1fr)',
+        justifyContent: 'center',
+      }}
+    >
+      <Header />
+      <Typography variant="h3" component="h3" align="center">
+        Find your Oompa Loompa
+      </Typography>
+      <Typography variant="h4" component="h4" align="center">
+        There are more than 100k
+      </Typography>
+      <SearchBar />
+      <List />
+    </Box>
+  )
 }
 
 export default Layout
